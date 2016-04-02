8297435941d1e5c57d5248af7a4a3c3a10e7606a
lurkas <lucasgarciadepolavieja@Lucass-MacBook-Air.local>
2016-04-02 03:26:55 -0400

    responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,177 @@
-/*var instaFeed = new Instafeed({
+
+var instaSearch = angular.module('root', []);
+instaSearch.controller('index', [
+    '$scope',
+    function ($scope) {
+        $scope.search;
+        var tag = $scope.search;
+        var loadButton = document.getElementById('load-more');
+        function loadInstagram(tagName) {
+            var feed = new Instafeed({
+                get: 'tagged',
+                tagName: tagName,
+                accessToken: '6678174.467ede5.205a03ebc4b74d4082823781c3149575',
+                target: 'instafeed',
+                sortBy: 'none',
+                limit: 33,
+                resolution: 'standard_resolution',
+                template: '<a href="{{link}}"><img src="{{image}}" /><div class="timestamp">{{model.created_time}}</div><div class="info"><p class="location"><i class="icon-location"></i>{{location}}</p><p><i class="icon-comment"></i>{{caption}}</p><br><ul><li class="icon-heart">{{likes}} likes<li class="icon-chat">{{comments}} comments</div></ul></div></a>',
+                filter: function(image) {
+
+    var date = new Date(image.created_time*1000);
+
+    d = date.getDate();
+    m = date.getMonth();
+/*    y = date.getFullYear();*/
+
+    var month_names = new Array ( );
+    month_names[month_names.length] = "Jan";
+    month_names[month_names.length] = "Feb";
+    month_names[month_names.length] = "Mar";
+    month_names[month_names.length] = "Apr";
+    month_names[month_names.length] = "May";
+    month_names[month_names.length] = "Jun";
+    month_names[month_names.length] = "Jul";
+    month_names[month_names.length] = "Aug";
+    month_names[month_names.length] = "Sep";
+    month_names[month_names.length] = "Oct";
+    month_names[month_names.length] = "Nov";
+    month_names[month_names.length] = "Dec";
+
+    var thetime = d + ' ' + month_names[m] + ' ';
+
+    image.created_time = thetime;
+
+    return true;
+  }
+            }).run();
+        };
+
+       
+        loadInstagram('nycsubwaymusic');
+        function clearFeed() {
+            document.getElementById('instafeed').innerHTML = '';
+        };
+
+
+        $scope.searchTag = function () {
+            clearFeed();
+            tag = $scope.search;
+            loadInstagram(tag);
+        };
+
+        /*data.enter().append(function(d) { return d })
+        .style("opacity",0).transition().duration(1000).style("opacity",1)
+*/
+        $scope.clear = function () {
+            $scope.search = '';
+            document.getElementById('search').focus();
+        };
+
+        instaSearch.directive('ngEnter', function () {
+            return function ($scope, element, attrs) {
+                element.bind('keydown keypress', function (event) {
+                    if (event.which === 13) {
+                        $scope.searchTag();
+                        event.preventDefault();
+                        return false;  
+                    }
+                });
+            };
+        });
+    }
+]);
+      
+   
+
+/*SORT BY*///////////
+
+$('#popular').click(function() {
+resort('none');
+});
+
+$('#recent').click(function() {
+resort('most-recent');
+});
+
+$('#like').click(function() {
+resort('most-liked');
+});
+
+$('#comment').click(function() {
+resort('most-commented');
+});
+
+function resort(sort) {
+
+$('#instafeed a').remove();
+
+    var instaFeed = new Instafeed({
+
+    get: 'tagged',
+    tagName: 'nycsubwaymusic',
+    userId: 6678174,
+    accessToken: '6678174.467ede5.205a03ebc4b74d4082823781c3149575',
+    target: 'instafeed',
+    sortBy: sort,
+    limit: 32,
+    resolution: 'standard_resolution',
+    template: '<a href="{{link}}"><img src="{{image}}" /><div class="timestamp">{{model.created_time}}</div><div class="info"><p class="location"><i class="icon-location"></i>{{location}}</p><p><i class="icon-comment"></i>{{caption}}</p><br><ul><li class="icon-heart">{{likes}} likes<li class="icon-chat">{{comments}} comments</div></ul></div></a>',
+    filter: function(image)
+    {
+
+     var date = new Date(image.created_time*1000);
+
+    d = date.getDate();
+    m = date.getMonth();
+/*    y = date.getFullYear();*/
+
+    var month_names = new Array ( );
+    month_names[month_names.length] = "Jan";
+    month_names[month_names.length] = "Feb";
+    month_names[month_names.length] = "Mar";
+    month_names[month_names.length] = "Apr";
+    month_names[month_names.length] = "May";
+    month_names[month_names.length] = "Jun";
+    month_names[month_names.length] = "Jul";
+    month_names[month_names.length] = "Aug";
+    month_names[month_names.length] = "Sep";
+    month_names[month_names.length] = "Oct";
+    month_names[month_names.length] = "Nov";
+    month_names[month_names.length] = "Dec";
+
+    var thetime = d + ' ' + month_names[m] + ' ';
+
+    image.created_time = thetime;
+
+    return true;
+  }
+
+
+}).run();
+
+$.fn.extend({
+  matchHeight: function(data){
+    var maxHeight = 0;
+    $(this).each(function() {
+       maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+    });
+   $(this).height(maxHeight);
+  }
+});
+
+}
+
+
+
+
+
+/*$('#like').click(function() {
+    console.log('#like');
+    $('#instafeed a').remove();
+
+var instaFeed = new Instafeed({
+
     get: 'tagged',
     tagName: 'nycsubwaymusic',
     userId: 6678174,
@@ -42,34 +215,51 @@
 }).run();
 
 $.fn.extend({
-    matchHeight: function (data) {
-        var maxHeight = 0;
-        $(this).each(function () {
-            maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-        });
-        $(this).height(maxHeight);
-    }
+  matchHeight: function(data){
+    var maxHeight = 0;
+    $(this).each(function() {
+       maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+    });
+   $(this).height(maxHeight);
+  }
 });
 
+});*/
 
-*/
 
 
-//when user clicks on one of the neighborhoods in #filter,
-//photos will filter based on that neighborhood selected
-$("ul").on("change", function() {
-	// Find out what the user selected and save to a variable
-	var selectedLi = $("ul").val();
 
-	// if selectedLocation = default
-	if (selectedLi === 'like') {
-		// Fade in all thumbnails
-		$("a").fadeIn(350);
-	// else
-	} else {
-		$("a").fadeOut(350, function () {
-			// Show all thumbnails with the corresponding class
-			$('#f-' + selectedLi).fadeIn(350);
-		});
-	}
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+  $('#like').click(function(event){
+    $('#instafeed a').remove()
+    .update('most-liked');
+  });*/
+
